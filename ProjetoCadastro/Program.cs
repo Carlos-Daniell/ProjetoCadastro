c0bf3bcc3c47631aac506fed08ef0f138f338372
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-06-04 23:15:39 -0300

    Criação dos metodos e organização do ponto de entrada + logica

--- ProjetoCadastro/Program.cs
+++ ProjetoCadastro/Program.cs
@@ -1,12 +1,41 @@
 ﻿
+using ProjetoCadastro.Telas;
 using System;
 
 namespace ProjetoCadastro {
     public class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("O que deseja fazer ? \n(1 = Cadastrar Cliente , \n2 = Listar Clientes, \n3 = Cadastrar Funcionário, \n4 = Listar Funcionários )");
-            
+            ManterCliente cliente = new ManterCliente();
+            ManterFuncionario func = new ManterFuncionario();
+            int opcoes = 0;
+
+            while (opcoes != 5) {
+
+                Console.WriteLine("O que deseja fazer ? \n 1 = Cadastrar Cliente, \n 2 = Listar Clientes, \n 3 = Cadastrar Funcionário, \n 4 = Listar Funcionários, \n 5 = Fechar Programa");
+                opcoes = int.Parse(Console.ReadLine());
+
+                switch (opcoes) {
+                    case 1:
+                        cliente.CadastrarCliente();
+                        break;
+                    case 2:
+                        cliente.ListarClientes();
+                        break;
+                    case 3:
+                        func.CadastrarFuncionario();
+                        break;
+                    case 4:
+                        func.ListarFuncionarios();
+                        break;
+                    default:
+                        Console.WriteLine("Comando Não Encontrado !");
+                        break;
+
+                }
+
+
+            }
 
         }
     }
---
+++ ProjetoCadastro/Telas/ManterFuncionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCadastro.Telas {
+    internal class ManterFuncionario {
+        public void CadastrarFuncionario() { }
+
+        public void ListarFuncionarios() { }
+    }
+}
